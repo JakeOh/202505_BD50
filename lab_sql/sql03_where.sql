7312d84371712b7eb0d3b856f5073abc5c6ee8ad
JakeOh <jake.oh.lecture@gmail.com>
2025-05-13 14:21:22 +0900

    where 조건절, 비교/논리 연산자

--- lab_sql/sql03_where.sql
+++ lab_sql/sql03_where.sql
@@ -36,16 +36,59 @@
  
 -- 급여가 2000 이상 3000 이하인 직원들의 이름, 업무, 급여를 출력.
 -- 정렬 순서: 급여 내림차순.
+select ename, job, sal
+from emp
+where sal >= 2000 and sal <= 3000
+order by sal desc;
+
+select ename, job, sal
+from emp
+where sal between 2000 and 3000
+order by sal desc;
 
 -- 10번 또는 20번 부서에서 근무하는 직원들의 부서번호, 이름, 급여를 출력.
 -- 정렬: (1) 부서번호 오름차순, (2) 급여 내림차순.
+select deptno, ename, sal
+from emp
+where deptno = 10 or deptno = 20
+order by deptno, sal desc;
+
+select deptno, ename, sal
+from emp
+where deptno in (10, 20)
+order by deptno, sal desc;
 
 -- 업무가 'CLERK'인 직원들의 이름, 업무, 급여를 출력. 이름순으로 출력.
+select ename, job, sal
+from emp
+where job = 'CLERK'
+order by ename;
+--> SQL에서 문자열은 작은따옴표('')를 사용.
+--> 비교하려는 문자열은 대/소문자를 구분.
 
 -- 업무가 'CLERK' 또는 'MANAGER'인 직원들의 이름, 업무, 급여를 출력.
 -- 정렬: (1) 업무, (2) 급여
+select ename, job, sal
+from emp
+where job = 'CLERK' or job = 'MANAGER'
+order by job, sal;
+
+select ename, job, sal
+from emp
+where job in ('CLERK', 'MANAGER')
+order by job, sal;
 
 -- 20번 부서에서 근무하는 'CLERK'의 모든 레코드(모든 컬럼)를 출력.
+select * from emp
+where deptno = 20 and job = 'CLERK';
 
 -- CLERK, ANALYST, MANAGER가 아닌 직원들의 사번, 이름, 업무, 급여를 사번순 출력.
+select empno, ename, job, sal
+from emp
+where job != 'CLERK' and job != 'ANALYST' and job != 'MANAGER'
+order by empno;
 
+select empno, ename, job, sal
+from emp
+where job not in ('CLERK', 'ANALYST', 'MANAGER')
+order by empno;
