cb656082e6e387967a07b0c21d0a7e0af7d62e3a
JakeOh <jake.oh.lecture@gmail.com>
2025-05-14 10:24:54 +0900

    replae, substr, nvl, nvl2

--- lab_sql/sql04_function.sql
+++ lab_sql/sql04_function.sql
@@ -29,5 +29,29 @@ select replace('smith', 'i', '-') from dual;
 select replace(ename, 'A', '*') from emp;
 
 -- substr(문자열 컬럼, 시작 인덱스, 자를 문자 개수)
-select substr('hello', 2, 2) from dual;
+select substr('hello', 2, 3) from dual;
 select substr(ename, 1, 2) from emp;
+
+-- substr(문자열, 시작 인덱스): 문자열에서 시작 인덱스 위치부터 끝까지 자름.
+select substr('Hello, SQL!', 5) from dual;
+
+-- emp 테이블에서 ename의 두번째 문자를 '*'로 바꿔서 출력.
+select ename, substr(ename, 2, 1) from emp;
+select 
+    replace(ename, substr(ename, 2, 1), '*') as "EMP_NAME"
+from emp;
+
+-- nvl(var, value): var의 값이 null이면 value를 반환(return)하고, 그렇지 않으면 var를 반환.
+select comm, nvl(comm, -1) from emp;
+
+-- nvl2(var, value1, value2): var의 값이 null이 아니면 value1를 반환하고, null이면 value2를 반환.
+select comm, nvl2(comm, 9999, 0) from emp;
+select comm, nvl2(comm, comm, -1) from emp;
+
+-- 사번, 이름, 급여, 수당, 연봉(sal * 12 + comm)을 출력.
+-- comm이 null인 경우는 comm을 0으로 계산,
+select
+    empno, ename, sal, comm,
+    nvl(sal, 0) * 12 + nvl(comm, 0) as "ANUAL_SAL"
+from emp
+order by ANUAL_SAL desc; -- select에서 설정한 alias(별명)을 order by에서 사용 가능.
