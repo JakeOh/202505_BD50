7535da4b54be831140ac9e4a4d550919fe9ac141
JakeOh <jake.oh.lecture@gmail.com>
2025-05-14 14:29:02 +0900

    group by query

--- lab_sql/sql05_function_groupby.sql
+++ lab_sql/sql05_function_groupby.sql
@@ -26,3 +26,67 @@ select
     min(sal) as "최솟값"
 from emp;
 
+-- 단일 행 함수와 다중 행 함수(그룹 함수)는 함께 사용할 수 없음!
+-- select count(comm), nvl(comm, 0) from emp;  --> 에러 발생
+-- select empno, count(empno) from emp;  --> 에러 발생
+
+/*
+ * 그룹별 쿼리
+ * (예) 부서별 급여 평균, 부서별 직원수, ...
+ * 
+ * select 컬럼, 함수, ...
+ * from 테이블, ...
+ * where 조건식(1)
+ * group by 컬럼(그룹을 묶을 수 있는 변수), ...
+ * having 조건식(2)
+ * order by 컬럼(정렬 기중이 된느 변수), ...;
+ *
+ * where 조건식(1): 그룹을 묶기 전에 조건을 만족하는 행들을 선택하기 위한 조건식.
+ * having 조건식(2): 그룹을 묶은 후에 조건을 만족하는 그룹들을 선택하기 위한 조건식.
+ *
+ * (주의) group by에서 지정하지 않은 컬럼 이름은 select할 수 없음.
+ */
+ 
+ -- 부서별 인원수
+ select deptno, count(*)
+ from emp
+ group by deptno
+ order by deptno;
+ 
+ -- 부서별 급여 평균
+ select 
+    deptno, round(avg(sal), 2) as "AVG_SAL"
+ from emp
+ group by deptno
+ order by deptno;
+ 
+ -- 급여가 null이 아닌 직원들 중에서 부서별 급여 평균
+ select 
+    deptno, round(avg(sal), 2) as "AVG_SAL"
+ from emp
+ where sal is not null
+ group by deptno
+ order by deptno;
+ 
+ -- 급여가 null이 아닌 직원들 중에서 부서별 직원수, 급여 평균/최솟값/최댓값
+ select 
+    deptno, 
+    count(*) as "COUNT_EMP", 
+    round(avg(sal), 2) as "AVG_SAL",
+    min(sal) as "MIN_SAL",
+    max(sal) as "MAX_SAL"
+ from emp
+ where sal is not null
+ group by deptno
+ order by deptno;
+ 
+ -- 업무가 null이 아닌 직원들 중에서 업무별 직원수, 급여 평균/표준편차
+ select
+    job,
+    count(*) as "COUNT_EMP",
+    round(avg(sal), 2) as "AVG_SAL",
+    round(stddev(sal), 2) as "STD_SAL"
+ from emp
+ where job is not null
+ group by job
+ order by job;
