24113fea3261606aeb42a52b31c0d69d3612d3b8
JakeOh <jake.oh.lecture@gmail.com>
2025-05-19 15:49:57 +0900

    create table, insert

---
+++ lab_sql/sql10_ddl.sql
@@ -0,0 +1,10 @@
+/*
+ * SQL 문장의 종류:
+ * 1. DQL(Data Query Language): select 문.
+ * 2. DDL(Data Definition Language): create, alter, truncate, drop 문.
+ * 3. DML(Data Manipulation Language): insert, update, delete 문.
+ * 4. TCL(Transaction Control Language): commit, rollback 문.
+ *
+ * 테이블 생성:
+ * 
+ */
