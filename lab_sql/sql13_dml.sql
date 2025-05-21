9112a3394b937bf6e4e39f96925a85a1a7373eb8
JakeOh <jake.oh.lecture@gmail.com>
2025-05-21 12:21:04 +0900

    dml, update, commit, rollback

--- lab_sql/sql13_dml.sql
+++ lab_sql/sql13_dml.sql
@@ -1,6 +1,7 @@
 /*
  * DML(Data Manipulation Language): insert, update, delete
  * DQL(Data Query Language): select
+ * TCL(Transaction Control Language): commit, rollback
  */
  
  -- INSERT INTO table_name VALUES (value, ...);
@@ -32,3 +33,27 @@
 select * from bonus;
 
 commit;
+
+
+-- update 문장: 테이블의 특정 컬럼의 값(들)을 수정(업데이트).
+-- UPDATE table_name 
+-- SET column_name = value, ... 
+-- WHERE condition_expression;
+-- where 절은 생략 가능. where 절을 생략하면 테이블의 모든 행들이 업데이트됨.
+
+--create table emp_copy 
+--as select * from emp;
+select * from emp_copy;
+
+-- 사번이 7369 직원의 급여를 1000, 수당을 100으로 업데이트
+update emp_copy
+set sal = 1000, comm = 100
+where empno = 7369;
+
+commit;  --> 직전 commit 이후의 변경된 모든 내용을 데이터베이스에 영구 저장.
+
+update emp_copy set sal = 1000;
+select * from emp_copy;  --> 모든 행의 sal 값이 변경됨.
+
+rollback; --> 직전 commit 이후의 변경된 내용을 원래대로 복원(원복).
+select * from emp_copy;  --> update 하기 전의 내용을 되돌리기.
