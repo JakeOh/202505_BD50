d3c7307989a73985419a683a00807c893d0bff1f
JakeOh <jake.oh.lecture@gmail.com>
2025-05-22 16:29:54 +0900

    치킨 주문 통화 분석

--- lab_sql/sql15_chicken.sql
+++ lab_sql/sql15_chicken.sql
@@ -13,4 +13,81 @@
  *    (6) 구별 성별 통화 건수의 평균
  *    (7) 요일별 구별 연령대별 통화 건수의 평균
  */
- 
+
+create table call_chicken (
+    call_date   date,
+    call_day    char(1 char),
+    gu          varchar2(5 char),
+    ages        varchar2(5 char),
+    gender      char(1 char),
+    calls       number(4)
+);
+
+select * from call_chicken;
+
+select distinct call_day from call_chicken;
+
+select count(*), count(call_day) from call_chicken;
+
+select distinct gu from call_chicken;
+
+select count(*), count(gu) from call_chicken;
+
+select distinct ages from call_chicken;
+
+select count(*), count(ages) from call_chicken;
+
+select distinct gender from call_chicken;
+
+select count(*), count(gender) from call_chicken;
+
+-- 1. 통화 건수의 최대/최소
+select max(calls), min(calls) from call_chicken;
+
+-- 2.
+select * from call_chicken
+where calls = (select max(calls) from call_chicken)
+    or calls = (select min(calls) from call_chicken)
+;
+
+select * from call_chicken
+where calls in (
+    select max(calls) as "CALLS" from call_chicken
+    union
+    select min(calls) as "CALLS" from call_chicken
+);
+
+-- 3. 요일별 통화 건수
+select
+    call_day, round(avg(calls),2) as CALLS
+from call_chicken
+group by call_day
+order by CALLS desc;
+
+-- 4. 연령대별
+select
+    ages, round(avg(calls),2) as CALLS
+from call_chicken
+group by ages
+order by CALLS desc;
+
+-- 5. 요일별, 연령대별
+select
+    call_day, ages, round(avg(calls),2) as CALLS
+from call_chicken
+group by call_day, ages
+order by CALLS desc;
+
+-- 6. 구별, 성별
+select
+    gu, gender, round(avg(calls),2) as CALLS
+from call_chicken
+group by gu, gender
+order by CALLS desc;
+
+-- 7. 요일, 구별, 연령대
+select
+    call_day, gu, ages, round(avg(calls),2) as CALLS
+from call_chicken
+group by call_day, gu, ages
+order by CALLS desc;
